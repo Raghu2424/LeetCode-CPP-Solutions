c227f20bdbdff2d7db2b59a268f1fdf2e817fd6b
Raghavendra <raghavendrakshetri@gmail.com>
2026-08-11 21:06:08 +0530

    Add Day 01 - Smallest Missing Integer solution

---
+++ Day-01-Smallest-Missing-Integer/Solution.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int sum = nums[0];
+
+        // Find the sum of the longest sequential prefix
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            } else {
+                break;
+            }
+        }
+
+        // Store all elements for fast lookup
+        unordered_set<int> numSet(nums.begin(), nums.end());
+
+        // Find the smallest missing integer >= sum
+        while (numSet.count(sum)) {
+            sum++;
+        }
+
+        return sum;
+    }
+};
